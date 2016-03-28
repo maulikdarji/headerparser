1afa68bb1d5352926a8ea3fc6803a77336b4d3fe
Maulik Darji <maulikdarji111@gmail.com>
2016-03-28 14:11:30 +0000

    modified output

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 var http = require("http");
 
 var server = http.createServer(function(req,res){
-  var ua='user-agent';
-  var ip='x-forwarded-for';
-  var lang='accept-language';
+  var ua=req.headers['user-agent'];
+  var ip=req.headers['x-forwarded-for'];
+  var lang=req.headers['accept-language'].split(";")[0].split(",")[0];
+  var browser='';
+  var os="";
+  
+  //Operating System Match
+  if(ua.match(/windows/i)){
+    os='Windows';
+  }
+  else if(ua.match(/android/i)){
+    os='Android';
+  }
+  else if(ua.match(/linux/i)){
+    os='Linux';
+  }
+  else if(ua.match(/apple/i)){
+    os="Apple";
+  }
+  else{
+    os="Unknown";
+  }
+  
+  //Browser Match
+  if(ua.match(/chrome/i)){
+    browser="Chrome";
+  }
+  else if(ua.match(/firefox/i)){
+    browser="Firefox";
+  }
+  else if(ua.match(/explorer/i)){
+    browser="Internet Explorer";
+  }
+  else{
+    browser="Unknown";
+  }
+  
   
   var op={
-    'ipaddress':req.headers[ip],
-    'language':req.headers[lang],
-    'Operating System':req.headers[ua]
+    'ipaddress':ip,
+    'language':lang,
+    'Operating System':os,
+    'Browser':browser
   }
   
     res.writeHead(200, {'Content-type':'application/json'});
